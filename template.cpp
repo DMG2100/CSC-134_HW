92494c6c677da41ca55084d64efe5b1c59b56b43
DMG2100 <tylandanner022705@gmail.com>
2025-02-09 17:26:29 +0000

    M2T1 Done

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+    // Say hi
+    cout << "Hello" << endl;
+    cout << "World!" << endl;
+    cout << endl;
+    return 0; // no errors
+
+}
