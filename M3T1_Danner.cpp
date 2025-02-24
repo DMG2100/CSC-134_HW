50852358a59b7f22a8a197d06d45457d3e555e95
DMG2100 <tylandanner022705@gmail.com>
2025-02-24 05:04:07 +0000

    M3T1_Danner Done

---
+++ M3T1_Danner.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+  //variables:
+    int widthOne;
+    int lengthOne;
+    int widthTwo;
+    int lengthTwo;
+    int areaOne;
+    int areaTwo;
+
+  //input ask:
+    cout << "Please enter the width and height of the first\n";
+    cout << "rectangle, separated by a space or new line" << endl;
+    cin >> widthOne >> lengthOne;
+
+    cout << "Please enter the width and height of the second\n";
+    cout << "rectangle, separated by a space or new line" << endl;
+    cin >> widthTwo >> lengthTwo;
+
+  //calculations:
+    areaOne = widthOne + lengthOne;
+    areaTwo = widthTwo + lengthTwo;
+
+  //result display:
+  cout << "Rectangle one has an area of " << areaOne << endl;
+  cout << "Rectangle two has an area of " << areaTwo << endl;
+
+  //determine which is bigger:
+  if (areaOne > areaTwo) {
+    cout << "Rectangle one is larger of the two." << endl;
+  } 
+  else if (areaOne < areaTwo) {
+    cout << "Rectangle two is larger of the two." << endl;
+  } 
+  else if (areaOne == areaTwo) {
+    cout << "Rectangle one and two are equal." << endl;
+  }
+  cout << "Thanks for using the program!" << endl;
+}
