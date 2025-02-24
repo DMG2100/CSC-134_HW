ebe8989e36304f0f917d5f1a654df33cbff6ef9e
DMG2100 <tylandanner022705@gmail.com>
2025-02-24 04:41:34 +0000

    M2HW1_Q4 Done

---
+++ M2HW1_Danner_Q4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+    //variables:
+    string school = "FTCC";
+    string team = "Trojans";
+    string letsGo = "Let's Go ";
+    string cheerOne;
+    string cheerTwo;
+    //code:
+    cheerOne = letsGo + school;
+    cheerTwo = letsGo + team;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+    return 0; // no errors
+
+}
