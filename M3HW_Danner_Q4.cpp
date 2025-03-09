90e704137559e4e15b1f805abea5206a8f32119c
DMG2100 <tylandanner022705@gmail.com>
2025-03-09 21:17:41 +0000

    M3HWQ4 Done

---
+++ M3HW_Danner_Q4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+    int random_number1 = rand() % 9 + 1;
+    int random_number2 = rand() % 9 + 1;
+    int user_input;
+    int answer = random_number1 + random_number2;
+
+    cout << "What is " << random_number1 << " + " << random_number2 << "?" << endl;
+    cin >> user_input;
+    if (user_input == answer)
+    {
+        cout << "Correct!";
+
+    }
+    else
+    {
+        cout << "Incorrect.";
+    }
+    return 0;
+
+}
