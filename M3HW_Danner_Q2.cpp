0bec08927d5b4413366e3641b5ebd624840f2bf3
DMG2100 <tylandanner022705@gmail.com>
2025-03-09 20:56:04 +0000

    M3HWQ2 Done

--- M3HW_Danner_Q2.cpp
+++ M3HW_Danner_Q2.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
-  
+   
 int main()
 {
-    // Say hi
-    cout << "Hello" << endl;
-    cout << "World!" << endl;
-    cout << endl;
-    return 0; // no errors
+    // variables
+    double order_total;
+    int order_type;         // apples bought variable
+    double order_price = 50;
+    double tax;       // price per apple variable
+    double tip = 10;
 
+    cout << "What type of order is this? Dine In (1) or Carry Out (2)? ";
+    cin >> order_type;
+
+    // receipt text
+    if (order_type == 1)
+    {
+        tax = order_price * 0.15;
+        order_total = (order_price + tax + tip);
+        cout << fixed << setprecision(2);
+        cout << "Meal price: $" << order_price << endl;
+        cout << fixed << setprecision(2);
+        cout << "Taxes: $" << tax << endl;
+        cout << fixed << setprecision(2);
+        cout << "Tip: $" << tip << endl;
+        cout << fixed << setprecision(2);
+        cout << "Order Total Price: $" << order_total << endl;
+        
+    }
+    else if (order_type == 2)
+    {
+        tax = order_price * 0.15;
+        order_total = order_price + tax;
+        cout << fixed << setprecision(2);
+        cout << "Meal price: $" << order_price << endl;
+        cout << fixed << setprecision(2);
+        cout << "Tax: $" << tax << endl;
+        cout << fixed << setprecision(2);
+        cout << "Order Total Price: $" << order_total << endl;
+    }
+    else
+    {
+        cout << "Invalid input!" << endl;
+    }
+
+    return 0;
 }
