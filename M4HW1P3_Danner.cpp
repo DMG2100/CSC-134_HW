a2dc5d7797d2846fcc75c408f3602305b792619a
DMG2100 <tylandanner022705@gmail.com>
2025-04-07 05:13:10 +0000

    M4HW1P3 Done

---
+++ M4HW1P3_Danner.cpp
@@ -0,0 +1,35 @@
+/*
+    Tylan Danner
+    level gold
+*/
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+    int number;
+    int multiplier = 1;
+    int sum;
+    cout << "Enter Number to Multiply from 1 to 12: ";
+    cin >> number;
+    if (number <= 12 && number >= 1)
+    {
+
+        while (multiplier <= 12)
+        {
+            sum = number * multiplier;
+            cout << number << " times " << multiplier << " equals " << sum << "." << endl;
+            multiplier++;
+            
+        }
+
+    } else
+
+    {
+        cout << "Invalid Number" << endl;
+        main();
+    }
+    return 0; // no errors
+
+}
